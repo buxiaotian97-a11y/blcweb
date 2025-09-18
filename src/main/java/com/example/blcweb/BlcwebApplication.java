3827f4772dfc87a6a7fe3d9ce466544de0295c48
小畑歩 <obata1101@icloud.com>
2025-09-18 17:25:31 +0900

    init spring boot app

---
+++ src/main/java/com/example/blcweb/BlcwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.blcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlcwebApplication.class, args);
+	}
+
+}
