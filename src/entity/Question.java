c27a572bae0cb963c17f152001692b092f6ae0dc
小畑歩 <obata1101@icloud.com>
2025-10-06 11:43:46 +0900

    質問取得APIを実装

--- src/entity/Question.java
+++
@@ -1,9 +0,0 @@
-@Entity @Table(name = "questions")
-public class Question {
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
-  String qtext;
-  String category;
-  int point;
-  boolean active;
-  // getter/setter
-}
