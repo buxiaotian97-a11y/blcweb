ef46a285ef0e285f636312c753791383018fd7bf
小畑歩 <obata1101@icloud.com>
2025-10-07 10:23:42 +0900

    不足アーキテクチャ（３つ）追加

--- src/main/java/com/example/blcweb/repository/QuestionRepository.java
+++ src/main/java/com/example/blcweb/repository/QuestionRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.blcweb.entity.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Optional<Question> findTopByOrderByIdAsc();                   // 最初の1件
-    Optional<Question> findFirstByIdGreaterThanOrderByIdAsc(Long id); // 次の1件
+
+    // 最初の1件を取得
+    Optional<Question> findTopByOrderByIdAsc();
+
+    // 次の1件を取得
+    Optional<Question> findFirstByIdGreaterThanOrderByIdAsc(Long id);
 }
